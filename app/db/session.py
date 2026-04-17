9acf23d198b8776150595ffdacef57c3b2e44005
ScandicCon <glhgygi.yiug@gmail.com>
2026-04-18 00:38:20 +0300

    add docker compose with postgres

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,20 @@
 from sqlmodel import create_engine, Session
+import os
 
-DATABASE_URL = 'sqlite:///database.db'
 
-engine = create_engine("sqlite:///database.db", echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine (
+        DATABASE_URL, 
+        echo=True,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        echo=True
+    )
 
 def get_session():
     with Session(engine) as session:
